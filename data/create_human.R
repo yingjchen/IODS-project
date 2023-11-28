9ddf9a634c5cdfef77eb864228309ae28cbdd19a
Chen <yingche@lm1-945-25189.local>
2023-11-28 18:31:03 +0100

    assignment 4

---
+++ data/create_human.R
@@ -0,0 +1,30 @@
+#R code for data wrangling for assignment 5
+
+library(readr)
+library(dplyr)
+hd <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/human_development.csv")
+gii <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/gender_inequality.csv", na = "..")
+
+str(hd)
+summary(hd)
+dim(hd) #195 observations and 8 variables
+
+
+str(gii)
+summary(gii)
+dim(gii) #195 observations and 10 variables
+
+colnames(gii)
+colnames(hd)
+
+#rename the variables with (shorter) descriptive names
+colnames(gii) <- c("GII Rank", "Country", "GII","Mat.Mor","Ado.Birth","Parli","Edu2.F","Edu2.M","Labo.F","Labo.M")
+colnames(hd) <- c("HDI Rank", "Country", "HDI","Life.Exp","Edu.Exp","Mean.Edu","GNI","Gni-HDI.Rank")
+
+
+gii$Edu2.ratio <- gii$Edu2.F / gii$Edu2.M
+gii$Labo.ratio <- gii$Labo.F / gii$Labo.M
+
+human <- inner_join(gii, hd, by = "Country")
+
+write.table(human, file="./data/human.csv",sep = ',',append = F,row.names = F,col.names = T)
