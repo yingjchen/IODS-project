54e7afe0c88da7c3f18b8c0da9c839044a784267
Chen <yingche@lm1-945-25189.local>
2023-12-04 19:39:34 +0200

    assignment 5

--- data/create_human.R
+++ data/create_human.R
@@ -28,3 +28,35 @@ gii$Labo.ratio <- gii$Labo.F / gii$Labo.M
 human <- inner_join(gii, hd, by = "Country")
 
 write.table(human, file="./data/human.csv",sep = ',',append = F,row.names = F,col.names = T)
+
+
+#chapter 5:  Dimensionality reduction techniques
+library(dplyr)
+library(readr)
+
+
+#load data
+human <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/human1.csv")
+#human <- read.csv('./data/human.csv', header = T, sep = ',', stringsAsFactors = F)
+
+
+#explore the structure and the dimensions
+dim(human) #195 observations and 19 variables
+str(human)
+colnames(human)
+
+
+#keep needed variables
+human <- human %>% select(c( "Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F" ))
+
+
+#Remove all rows with missing values 
+human_ <- human[complete.cases(human), ]
+
+
+#Remove the observations which relate to regions instead of countries.
+human_ <- human_[1:155, ]
+dim(human_) #[1] 155   9
+
+#save data
+write.table(human_, file="./data/human.csv",sep = ',',append = F,row.names = F,col.names = T)
